d1f58d11966ffd28a307d43b77d92d939fa041a9
Santiago Badino <badino.santi@gmail.com>
2023-02-26 16:30:41 -0300

    form template

--- public/frontEnd/script.js
+++ public/frontEnd/script.js
@@ -1,6 +1,13 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
+// Get form data
+const nameInput = document.querySelector("#name")
+const email = document.querySelector("#email")
+const message = document.querySelector("#message")
+const success = document.querySelector("#success")
+const errorNodes = document.querySelectorAll(".error")
+
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
@@ -10,3 +17,49 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
   hamburger.classList.remove("active");
   navMenu.classList.remove("active");
 }))
+
+// Validate form data
+function validateForm() {
+
+  clearMessages();
+  let errorFlag = false;
+
+  if(nameInput.value.length < 1) {
+    errorNodes[0].innerText = "Name cannot be blank";
+    nameInput.classList.add("error-border");
+    errorFlag = true;
+  }
+
+  if(!emailIsValid(email.value)) {
+    errorNodes[1].innerText = "Invalid email address";
+    email.classList.add("error-border");
+    errorFlag = true;
+  }
+
+  if(message.value.length < 1) {
+    errorNodes[2].innerText = "Please enter message";
+    message.classList.add("error-border");
+    errorFlag = true;
+  }
+
+  if(!errorFlag) {
+    success.innerText = "Success!";
+  }
+}
+
+// Clear error / success messages
+function clearMessages(){
+  for(let i = 0; i < errorNodes.length; i++){
+    errorNodes[i].innerText = "";
+  }
+  success.innerText = "";
+  nameInput.classList.remove("error-border");
+  email.classList.remove("error-border");
+  message.classList.remove("error-border");
+}
+
+// Check if email is valid
+function emailIsValid(email) {
+  let pattern = /\S+@\S+\.\S+/;
+  return pattern.test(email);
+}
